e1186f35d99946fa4479a02d886c52f6d5066a5d
hhoanganh <hhoanganh@users.noreply.github.com>
2025-06-19 10:55:41 +0700

    Update BloodDemandSection.tsx

--- src/components/BloodDemandSection.tsx
+++ src/components/BloodDemandSection.tsx
@@ -55,7 +55,7 @@ const BloodDemandSection = () => {
         </div>
 
         {/* Blood Type Grid - Organic Cluster */}
-        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-6 md:gap-8 max-w-6xl mx-auto">
+        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-6 md:gap-8 max-w-4xl mx-auto">
           {bloodTypes.map((blood, index) => (
             <div 
               key={blood.type}
